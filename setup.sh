4bb63ba888865112bba5f0234a58abc753ffa883
Sergio Chica <sergio.chica@uc3m.es>
2020-04-02 23:42:18 +0200

    Simplified regex to match every character and replace by real ip

--- setup.sh
+++ setup.sh
@@ -103,7 +103,7 @@ EOF
     ip=$($d inspect -f '{{.NetworkSettings.Networks.bridge.IPAddress}}' $vpns)
 
     if [ -f $vpnf ]; then
-        sed -i "s/\([a-z]\+\)[^\s]\+\s\+\([0-9]\+\s[a-z]\+\)/\1 $ip \2/" $vpnf
+        sed -i "s/remote.*1194 udp/remote "$ip" 1194 udp/" $vpnf
     fi
 }
 
